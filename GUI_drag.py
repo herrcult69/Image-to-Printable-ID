65b21c1050a14cb00504b44bbd1dd73a0aa6dbde
Herr Cult <10423041@student.vgu.edu.vn>
2024-05-16 13:32:22 +0700

    Testing completed, un orginzed

--- GUI_drag.py
+++ GUI_drag.py
@@ -1,25 +1,14 @@
-import PIL.Image
 from tkinter import *
+class GUI:
+    def __init__(self, root, width= 1280, height= 720):
+        self.width = width
+        self.height = height
+        self.canvas = Canvas(root, width=self.width, height=self.height)
 
-def drag(event):
-    canvas.coords(image, event.x, event.y)
-    canvas.itemconfig(text, text=f"Coordinate: x:{str(event.x)}, y:{str(event.y)}")
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
     
-    
-img = PIL.Image.open("inputs/unwarp.jpg")
-w, h = img.size
-x, y = w/2, h/2
-root = Tk()
-root.title("GUI_TEST")
-root.geometry(f"{w}x{h}")
-
-#Make black canvas, which is the place where objects are drawn
-canvas = Canvas(root, width=w, height=h, bg= "white")
-img = PhotoImage(file="saves/new_image.png")
-image = canvas.create_image(x, y, image= img)
-text = canvas.create_text(300, 50, text="Drag for Coords", fill="black", font=('Helvetica 15 bold'))
-canvas.pack(pady= 20)
-canvas.bind("<B1-Motion>", drag)
-
-root.mainloop()
-""" Message to self, tomorrow, add 4 round widgets and move them, if mouse coords are near a round dot, drag it """
